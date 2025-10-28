544c5a08bad31a3fa34ec708e8984814823903f0
omerabin <fuzzing603@gmail.com>
2025-10-28 11:49:42 +0200

    no need to use json, removed

--- apps/parking-synchronizer/src/services/fetcher.ts
+++ apps/parking-synchronizer/src/services/fetcher.ts
@@ -10,20 +10,17 @@ const PUBLIC_PARKINGS_API_URL = '556/query?where=1%3D1&outFields=*&f=json';
 export const getAhuzotHofParkingsFromAPI = async () =>
   apiRequestGenerator<any>({
     url: `${process.env.OPEN_DATA_TLV_API_URL}/${AHUZOT_HOF_PARKINGS_API_URL}`,
-    saveToFile: 'ahuzotHofParkings.json',
     schema: AhuzotHofParkingSchema,
   });
 
 export const getPrivateParkingsFromAPI = async () =>
   apiRequestGenerator<any>({
     url: `${process.env.OPEN_DATA_TLV_API_URL}/${PRIVATE_PARKINGS_API_URL}`,
-    saveToFile: 'privateParkings.json',
     schema: PrivateParkingSchema,
   });
 
 export const getPublicParkingsFromAPI = async () =>
   apiRequestGenerator<any>({
     url: `${process.env.OPEN_DATA_TLV_API_URL}/${PUBLIC_PARKINGS_API_URL}`,
-    saveToFile: 'publicParkings.json',
     schema: PublicParkingSchema,
   });
--- apps/parking-synchronizer/src/utils/apiRequestGenerator.ts
+++ apps/parking-synchronizer/src/utils/apiRequestGenerator.ts
@@ -6,26 +6,17 @@ import { ZodSchema } from 'zod';
 
 interface ApiRequestOptions<T> {
   url: string;
-  saveToFile?: string;
   schema?: ZodSchema<T>;
 }
 
 export const apiRequestGenerator = async <T>({
   url,
-  saveToFile,
   schema,
 }: ApiRequestOptions<T>): Promise<T> => {
   try {
     const { data } = await axios.get<T>(url);
 
     const validatedData = schema ? schema.parse(data) : data;
-
-    if (saveToFile) {
-      const fullPath = path.resolve('src/temp', saveToFile);
-      await fs.writeFile(fullPath, JSON.stringify(data, null, 2), 'utf-8');
-      console.log(`✅ Data saved to ${fullPath}`);
-    }
-
     return validatedData;
   } catch (error) {
     console.error(`❌ Failed to fetch data from ${url}:`, error);
